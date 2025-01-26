9ff1dbeeff1aa772c1d9c976f570e14e0f203464
Viktor Kovács <viktor.kovacs02@gmail.com>
2025-01-26 11:21:54 +0100

    feat: Ability to create admin user

---
+++ app/Console/Commands/CreateAdminUserCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Role\Enums\RoleEnum;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class CreateAdminUserCommand extends Command
+{
+    protected $signature = 'app:create-admin-user-command {name} {email} {password?}';
+
+    protected $description = 'Creates admin user.';
+
+    public function handle(): int
+    {
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+
+        $password ??= Str::password();
+
+
+        $user = User::create([
+            'role' => RoleEnum::ADMIN->value,
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info("Created admin user {$user->name} with password: {$password}");
+
+        return Command::SUCCESS;
+    }
+}
