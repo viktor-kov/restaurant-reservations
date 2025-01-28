ffef1ec3a7927d1c53ca57874eb02768c8b4c3d0
Viktor Kovács <viktor.kovacs02@gmail.com>
2025-01-28 20:44:27 +0100

    feature: Auth routes are removed

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,10 @@ use App\Http\Controllers\Admin\Reservation\DeleteReservationController;
 use App\Http\Controllers\Admin\Reservation\ListReservationController;
 use App\Http\Controllers\Admin\Reservation\ShowReservationController;
 use App\Http\Controllers\Page\HomepageController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomepageController::class)->name('homepage');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 Route::group(['middleware' => ['auth', IsAdminMiddleware::class], 'prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('/reservations', ListReservationController::class)->name('reservations.list');
--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -19,16 +19,16 @@ class PasswordResetTest extends TestCase
     //        $response->assertStatus(200);
     //    }
 
-    public function test_reset_password_link_can_be_requested(): void
-    {
-        Notification::fake();
-
-        $user = User::factory()->create();
-
-        $this->post('/forgot-password', ['email' => $user->email]);
-
-        Notification::assertSentTo($user, ResetPassword::class);
-    }
+    //    public function test_reset_password_link_can_be_requested(): void
+    //    {
+    //        Notification::fake();
+    //
+    //        $user = User::factory()->create();
+    //
+    //        $this->post('/forgot-password', ['email' => $user->email]);
+    //
+    //        Notification::assertSentTo($user, ResetPassword::class);
+    //    }
 
     //    public function test_reset_password_screen_can_be_rendered(): void
     //    {
@@ -47,27 +47,27 @@ class PasswordResetTest extends TestCase
     //        });
     //    }
 
-    public function test_password_can_be_reset_with_valid_token(): void
-    {
-        Notification::fake();
-
-        $user = User::factory()->create();
-
-        $this->post('/forgot-password', ['email' => $user->email]);
-
-        Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
-            $response = $this->post('/reset-password', [
-                'token' => $notification->token,
-                'email' => $user->email,
-                'password' => 'password',
-                'password_confirmation' => 'password',
-            ]);
-
-            $response
-                ->assertSessionHasNoErrors()
-                ->assertRedirect(route('login'));
-
-            return true;
-        });
-    }
+    //    public function test_password_can_be_reset_with_valid_token(): void
+    //    {
+    //        Notification::fake();
+    //
+    //        $user = User::factory()->create();
+    //
+    //        $this->post('/forgot-password', ['email' => $user->email]);
+    //
+    //        Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
+    //            $response = $this->post('/reset-password', [
+    //                'token' => $notification->token,
+    //                'email' => $user->email,
+    //                'password' => 'password',
+    //                'password_confirmation' => 'password',
+    //            ]);
+    //
+    //            $response
+    //                ->assertSessionHasNoErrors()
+    //                ->assertRedirect(route('login'));
+    //
+    //            return true;
+    //        });
+    //    }
 }
