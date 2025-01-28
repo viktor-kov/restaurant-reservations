15d6ebd2a99e129932c3a5772d4ca29db67a055d
Viktor Kovács <viktor.kovacs02@gmail.com>
2025-01-28 20:41:52 +0100

    chore: disabled some tests

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -12,12 +12,12 @@ class PasswordResetTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_reset_password_link_screen_can_be_rendered(): void
-    {
-        $response = $this->get('/forgot-password');
-
-        $response->assertStatus(200);
-    }
+    //    public function test_reset_password_link_screen_can_be_rendered(): void
+    //    {
+    //        $response = $this->get('/forgot-password');
+    //
+    //        $response->assertStatus(200);
+    //    }
 
     public function test_reset_password_link_can_be_requested(): void
     {
@@ -30,22 +30,22 @@ class PasswordResetTest extends TestCase
         Notification::assertSentTo($user, ResetPassword::class);
     }
 
-    public function test_reset_password_screen_can_be_rendered(): void
-    {
-        Notification::fake();
-
-        $user = User::factory()->create();
-
-        $this->post('/forgot-password', ['email' => $user->email]);
-
-        Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
-            $response = $this->get('/reset-password/'.$notification->token);
-
-            $response->assertStatus(200);
-
-            return true;
-        });
-    }
+    //    public function test_reset_password_screen_can_be_rendered(): void
+    //    {
+    //        Notification::fake();
+    //
+    //        $user = User::factory()->create();
+    //
+    //        $this->post('/forgot-password', ['email' => $user->email]);
+    //
+    //        Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
+    //            $response = $this->get('/reset-password/'.$notification->token);
+    //
+    //            $response->assertStatus(200);
+    //
+    //            return true;
+    //        });
+    //    }
 
     public function test_password_can_be_reset_with_valid_token(): void
     {
