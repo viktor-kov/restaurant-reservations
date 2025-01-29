a10fa50f8724d347b39b5296dc1d8485be30578e
Viktor Kovács <viktor.kovacs02@gmail.com>
2025-01-29 20:05:39 +0100

    fix: background image

--- config/restaurant.php
+++ config/restaurant.php
@@ -2,7 +2,7 @@
 
 return [
     'tables_count' => 2,
-    'max_seats_per_table' => 5,
+    'max_seats_per_table' => 10,
     'weekends' => false,
     'first_available_time' => '12:00',
     'last_available_time' => '24:00',
